4b2364191975420c1340fcfcc27b56677dc691ed
pratikpr <89504885+prpr4667@users.noreply.github.com>
2023-04-23 19:45:47 -0600

    home page intro + photo with socials

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/" className="home__social-icon" target='_blank'>
+            <i class="uil uil-instagram-alt"></i>
+        </a>
+        <a href="https://www.twitter.com" className="home__social-icon" target='_blank'>
+            <i class="uil uil-twitter"></i>
+        </a>
+        <a href="https://www.github.com" className="home__social-icon" target='_blank'>
+            <i class="uil uil-github"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
